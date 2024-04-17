347e8047f4e8e5652cc61a41e911c6a19919bddb
zhulduzz <123058905+zhulduzz@users.noreply.github.com>
2024-04-18 02:21:02 +0600

    Add files via upload

---
+++ lab7/hackerRank/3.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    summa = a + b
+    difference = a - b
+    product = a * b
+
+    print(summa)
+    print(difference)
+    print(product)
---
+++ lab7/hackerRank/4.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    division = a / b
+    print(int(division))
+    print((float(division)))
